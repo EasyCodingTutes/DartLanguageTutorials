1823ebefd996d3ea246398aa8f5a68a84832084f
EasyCodingTutes <easycodingtutes@gmail.com>
2022-09-07 22:47:34 +0530

    Added program to Dart comments

---
+++ DartComments.dart
@@ -0,0 +1,18 @@
+void main() {
+    // Single line comment
+    print("Single line comment.");
+
+    /* This is the example of multi-line comment 
+    This will print the given statement on screen 
+    main() {
+      print("This is inside multline comment.");
+    }*/
+    print("Multi line Comment is above this line.");
+
+    ///This is   
+    ///the example of   
+    ///multi-line comment  
+    ///This will print the given statement on screen.  
+    print("Documentational Comment is above this line.");
+
+}
