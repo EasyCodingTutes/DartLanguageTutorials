4c1462ab85d3f4509719db6fe3e5a1fb7f22fefc
EasyCodingTutes <easycodingtutes@gmail.com>
2022-09-29 15:37:16 +0530

    Added code for Dart Sets

--- DartSetsMethods.dart
+++ DartSetsMethods.dart
@@ -6,18 +6,25 @@ void main() {
   Set b = {2,3,7,8};
 
   // add(): It inserts a value at the end of the list
+  print(vowels.add("A"));
 
   // addAll(): This method is used to insert multiple values to the list
+  vowels.addAll({'E', 'I', 'O', 'U'});
+  print(vowels);
 
   // elementAt(): It is used to access the item by passing its specified index position.
+  print(vowels.elementAt(6));
 
   // contains(): It finds an element in the set.
+  print(vowels.contains("A"));
 
   // remove(): It eliminate or remove an element from the given set.
   vowels.remove('a');
   print(vowels);
 
   // toList(): It converts set Object to list Object
+  List vowelsList = vowels.toList();
+  print(vowelsList);
 
   // union(): It combine the value of the two given sets a and b.
   Set c = a.union(b);
--- DartSetsProperties.dart
+++ DartSetsProperties.dart
@@ -4,17 +4,24 @@ void main() {
   Set vowels = {'a', 'e', 'i', 'o', 'u'};
 
   // first: It is used to get the first element in the given set.
+  print(vowels.first);
 
   // isEmpty: If the set does not contain any element, it returns true.
+  print(vowels.isEmpty);
 
   // isNotEmpty: If the set contains at least one element, it returns true
+  print(vowels.isNotEmpty);
 
   // length: It returns the length of the given set.
+  print(vowels.length);
 
   // last: It is used to get the last element in the given set.
+  print(vowels.last);
 
   // hashcode: It is used to get the hash code for the corresponding object.
+  print(vowels.hashCode);
 
   // single: It is used to check whether a set contains only one element.
+  print(vowels.single);
 
 }  
